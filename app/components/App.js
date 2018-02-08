90a08b45eb31ba5a6b8c198cb7ef801a2b4ac549
enrique novoa <luisenriquenovoa@gmail.com>
2018-02-07 20:51:50 -0800

    4th

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var Home = require('./Home');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    )
+  }
+}
+
+module.exports = App;
