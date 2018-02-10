145871a7632aa61e9c25434d59dd8f595c9bfdd8
enrique novoa <luisenriquenovoa@gmail.com>
2018-02-09 21:08:51 -0800

    11th

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,11 +1,39 @@
 var axios = require('axios');
 
+var urlBase = "http://api.openweathermap.org/data/2.5/";
 var API_KEY = "2d33c7b8363de03942cccc98d15bbdcd";
-var city;
 
-function getCity(city) {
-  return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&type=accurate&APPID=${API_KEY}`)
-    .then(function(weather){
-      return weather.data
-    })
+function getQueryStringData(city) {
+  return {
+    q: city,
+    type: accurate,
+    APPID: API_KEY,
+    cnt: 5
+  }
 }
+
+function getCurrentWeather(city) {
+  var queryStringData = getQueryStringData(city);
+  var url = prepUrl('weather', queryStringData);
+
+  return axios.get(url)
+    .then((weatherData) => {
+      return weatherData.data
+    });
+}
+
+function getForecast(city) {
+  var queryStringData = getQueryStringData(city);
+  var url = prepUrl('forecast/daily', queryStringData);
+
+  return axios.get(url)
+    .then((forecastData) => {
+      return forecastData.data
+    });
+
+}
+
+module.exports = {
+  getCurrentWeather: getCurrentWeather,
+  getForecast: getForecast
+};
