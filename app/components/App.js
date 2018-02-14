c5e8d10c876c5f71169453c7c2a74afc1cbb6ff3
enrique novoa <luisenriquenovoa@gmail.com>
2018-02-13 20:45:19 -0800

    20th

--- app/components/App.js
+++ app/components/App.js
@@ -1,23 +1,50 @@
 var React = require('react');
-var Nav = require('./Nav');
-var Home = require('./Home');
+var ZipCode = require('./ZipCode');
 var Forecast = require('./Forecast');
 var ReactRouter = require('react-router-dom');
 var BrowserRouter = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
       <BrowserRouter>
-        <div className="container">
-          <Route>
-            <Nav />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/forecast" component={Forecast}/>
+        <div className='container'>
+          <Route render={function (props) {
+            return (
+              <div className='navbar'>
+                <h1>Clever Title</h1>
+                <ZipCode
+                  direction='row'
+                  onSubmitZipcode={function(city){
+                    props.history.push({
+                      pathname: 'forecast',
+                      search: '?city=' + city
+                    });
+                  }}
+                  zipcode={123} />
+              </div>
+            )
+          }} />
+
+          <Route exact path='/' render={function (props) {
+            return (
+              <div className='home-container' style={{backgroundImage: "url('app/images/pattern.svg')"}}>
+                <h1 className='header'>Enter a City and State</h1>
+                <ZipCode
+                  direction='column'
+                  onSubmitZipcode={function (city) {
+                    props.history.push({
+                      pathname: 'forecast',
+                      search: '?city=' + city
+                    })
+                  }}
+                  zipcode={123} />
+              </div>
+            )
+          }} />
+
+          <Route path='/forecast' component={Forecast} />
         </div>
       </BrowserRouter>
     )
